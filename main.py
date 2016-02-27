b10d9d3176eac26088b5100fd275cca22004ea55
merkuria <merkuria@o2.pl>
2016-02-27 15:57:50 +0100

    My Django Girls app, first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+"""print('Hello, Django girls!')
+if 5>2:
+    print('cokolwiek')
+if 5>2:
+    print('5 jest jednak wieksze od 2')
+else:
+    ('5 nie jest większe od 2')
+name='Sonja'
+if name=='Ola':
+    print('Hej Ola!')
+elif name=='Sonja':
+    print('Hej Sonja!')
+else:
+    print('Hej nieznajoma!')
+glosnosc =57
+if glosnosc < 20:
+    print("Prawie nic nie slychac.")
+elif 20<= glosnosc<40:
+    print("O, muzyka leci w tle")
+elif 40<=glosnosc<60:
+    print ("idealnie, moge uslyszec wszystkie detale")
+elif 60<=glosnosc<80:
+    print ("Dobre na imprezy")
+elif 80<=glosnosc<100:
+    print("troszeczke za glosno!")
+else:
+    print("ojoj! moje uszy!:(")
+def hej():
+    print('Hej')
+    print('jak sie masz?')
+hej()
+def hej(imie):
+    if imie == 'Ola':
+        print('Hej Ola!')
+    elif imie == 'Sonja':
+        print('Hej Sonja!')
+    else:
+        print('Hej nieznajoma!')
+
+hej('Wojtek')
+def hej(imie):
+    print('Hej '+imie+ '!')
+hej("Rachel")
+
+
+def hej(imie):
+    print('Hej '+imie+ '!')
+
+dziewczyny = ['Ania', 'Marysia','Karolina', 'Asia','You']
+for imie in dziewczyny:
+    hej(imie)
+    print('Kolejna dziewczyna')
+for i in range (0,6):
+    print(i)"""
